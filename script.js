3e6e5f452cad5ad7c20b89b728599e43d4d49936
mungaii <theforexer1@gmail.com>
2024-04-25 12:21:55 +0300

    Added opacity effect: <p></p> element, script.js

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const jokeBody = document.getElementById("joke");
 const clickBtn = document.getElementById("btn");
 
 let getValue = () => {
-    fetch("https://v2.jokeapi.dev/joke/Any")
+    // Adding a cache-busting parameter to the url
+    const url = "https://v2.jokeapi.dev/joke/Any";
+
+    jokeBody.classList.remove("fade");
+    fetch(url)
     .then(response => response.json())
     .then(result =>{ 
         // Updating the text content within jokeBody
         jokeBody.textContent = result.joke;
+        jokeBody.classList.add("fade");
         })
     .catch(error => console.log("Error fetching code", error))
 } 
