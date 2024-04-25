cf76af47dcb2b17f79d561d9695d43e7605bf3df
mungaii <theforexer1@gmail.com>
2024-04-25 12:00:54 +0300

    Modifying code: getValue function, script.js

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-// const emojiBody = document.getElementById("emoji");
-// const jokeBody = document.getElementById("joke");
-// const clickBtn = document.getElementById("btn");
-
-// let getValue = () => {
-//     fetch("https://v2.jokeapi.dev/joke/Any")
-//     .then(response => response.json())
-//     .then(result => emojiBody = `${result.joke}`)
-// } 
-
-// clickBtn.addEventListener("click", getValue);
-// getValue();
-
-
-
-
-const emojiBody = document.getElementById("emoji");
+const emojiBody = document.querySelector(".emoji");
 const jokeBody = document.getElementById("joke");
 const clickBtn = document.getElementById("btn");
 
 let getValue = () => {
     fetch("https://v2.jokeapi.dev/joke/Any")
     .then(response => response.json())
-    .then(result => {
-        emojiBody.textContent = result.joke;
-    })
-    .catch(error => {
-        console.error('Error fetching joke:', error);
-    });
-}; 
+    .then(result =>{ 
+        // Updating the text content within jokeBody
+        jokeBody.textContent = result.joke;
+        })
+    .catch(error => console.log("Error fetching code", error))
+} 
 
 clickBtn.addEventListener("click", getValue);
-getValue();
+// Initial joke fetch when page loads
+getValue();   
+
+
+
+
