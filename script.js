dce9777fd0d2ba984e4e55f31d0c6e0e8a5a4a63
ianthedeveloper <thedeveloperian@gmail.com>
2024-05-11 20:53:07 +0300

    Formatting code, script.js

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-const jokeBody=document.getElementById("joke");
-const clickBtn=document.getElementById("btn");
-
-let getJoke=()=>{
-    const url="https://v2.jokeapi.dev/joke/Any";
-    jokeBody.classList.remove("fade");
-    fetch(url)
-    .then(response=>response.json())
-    .then(result=>{jokeBody.textContent=result.joke;
-    jokeBody.classList.add("fade")})
-    .catch(error=>console.log("Ooops! There was an error",error))
-    console.log("Fetching you a JOKE")
-    }
-
-function handleButtonClick(){
-    getJoke()
-}
-
-clickBtn.addEventListener("click",handleButtonClick);
-getJoke()
+const jokeBody=document.getElementById("joke");const clickBtn=document.getElementById("btn");let getJoke=()=>{const url="https://v2.jokeapi.dev/joke/Any";jokeBody.classList.remove("fade");fetch(url).then(response=>response.json()).then(result=>{jokeBody.textContent=result.joke;jokeBody.classList.add("fade")}).catch(error=>console.log("Ooops! There was an error",error))
+console.log("Fetching you a JOKE")}function handleButtonClick(){getJoke()}clickBtn.addEventListener("click",handleButtonClick);getJoke()
